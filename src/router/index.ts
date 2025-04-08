8eedcc0165619d8027ba1d0c3217d3fc4d69e13c
kaz-olsc <agg0097@alu.medac.es>
2025-04-08 14:11:00 +0200

    Creada estructura base de Router (#5)

---
+++ src/router/index.ts
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import LoginPage from '../pages/LoginPage.vue'
+
+
+
+const routes = [
+
+  {path: '/', component: LoginPage},
+  
+]
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
