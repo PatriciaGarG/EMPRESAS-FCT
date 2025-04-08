ac8e9af12ca74086a9d9a49877fd9c1bb05750be
Patricia GarGon <patriciazx9@gmail.com>
2025-04-08 14:11:49 +0200

    Base hecha de la página sin lógica .ts

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
+import CompanyInformation from './components/services/companyInformation/companyInformation.vue';
+
+
 </script>
 
 <template>
-  
+  <div>
+    <CompanyInformation/>
+  </div>
 </template>
 
