e9cd7c5fb0a612aa252c6ad90ad797a4f3f2e73b
kaz-olsc <agg0097@alu.medac.es>
2025-04-24 11:32:48 +0200

    Ajustando lógica

--- src/pages/CompanyDashboardPage.vue
+++ src/pages/CompanyDashboardPage.vue
@@ -1,13 +1,8 @@
 <script setup lang="ts">
   import { provide, onMounted } from 'vue';
   import { useCompany } from '../composables/useCompany';
-<<<<<<< HEAD
   import HeaderDashboard from '../components/common/HeaderDashboard.vue';
   import CompanyDashboard from '../components/dashboard/company-dashboard/CompanyDashboard.vue';
-=======
-  import tableCompanyDashboard from '../components/dashboard/company-dashboard/tableCompanyDashboard.vue';
-  import HeaderDashboard from '../components/common/HeaderDashboard.vue';
->>>>>>> 7f46175626dc68614d742467db4dcd3a21d51ed3
 
   const { company, getCompany } = useCompany();
 
@@ -21,13 +16,7 @@
 
 <template>
   <body class="flex flex-col h-screen w-screen bg-gray-light">
-<<<<<<< HEAD
     <HeaderDashboard />
     <CompanyDashboard />
-=======
-    <headerDashboard />
-    <AddCompanyButton/>
-    <tableCompanyDashboard />
->>>>>>> 7f46175626dc68614d742467db4dcd3a21d51ed3
   </body>
 </template>
